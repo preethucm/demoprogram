a8b7d2697907aa21c14b103bed8ea526fdc1d3e0
preethucm <107473571+preethucm@users.noreply.github.com>
2022-06-14 16:12:25 +0530

    Add files via upload

---
+++ Employee.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+class Employee:
+    def __init__(name,age,salary,gender):
+        self.name("name")
+        self.age("age")
+        self.salary("salary")
+        self.gender("gender")
+    def employee_details(self):
+        print("Name of the employee",name)
+        print("Age of the employee",age)
+        print("Salary is",salary)
+        print("Gender is",gender)
+    e1=Employee('Sid',31,50000,'Male')
+    e1=employee_details()
